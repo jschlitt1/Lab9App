30e6f617bbd4a2108e1158fc5592b17f3e621a77
jschlitt1 <47009980+jschlitt1@users.noreply.github.com>
2020-04-08 15:31:54 -0500

    first Commit
    should be only, did this after I finish, would only be for quick fixes

---
+++ Homework9App/Homework9App/Homework9App/AgePage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Homework9App
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AgePage : ContentPage
+    {
+        public AgePage()
+        {
+            InitializeComponent();
+        }
+        
+        public void CalculateYears(Object sender, EventArgs args)
+        {
+            int length = Convert.ToInt32(Age.Text) - 16;
+            Output.Text = "You have been able to legally drive for " + length + " years.";
+        }
+    }
+}
